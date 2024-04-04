367b1d2d69bf0e1a6a4ecc197fbd58be1b049501
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-04 13:21:44 +0300

    Добавьте файлы проекта.

---
+++ Lesson-3.3.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string login;
+    string password;
+
+    cout << "Введите логин: ";
+    cin >> login;
+    cout << "Введите пароль: ";
+    cin >> password;
+    cout << "-----------------";
+    cout << endl << login << ", Вы успешно вошли в проект.\n";
+
+    return 0;
+}
+
